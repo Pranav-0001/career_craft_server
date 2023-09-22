f60443ca626248556502fbae97ddf9910f7b6d17
Pranav-0001 <pranavchakkalakkal@gmail.com>
2023-09-22 14:13:32 +0530

    code clean up

--- src/Interfaces/Routes/messageRoute.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const messageContollers_1 = require("../controllers/messageContollers");
-const router = (0, express_1.Router)();
-router.post('/send', messageContollers_1.sendMessage);
-router.get('/:chatId', messageContollers_1.getMessagesByChatId);
-exports.default = router;
